6018c89fe54f503f9d623c698258ed3376d9bdec
Daniel Rodríguez Gamboa <daniel@n56v>
2016-06-03 21:50:58 -0600

    Awebo Parte2 | The empire strikes back |

--- hack/registro.php
+++ hack/registro.php
@@ -14,9 +14,12 @@
     <title>Empleo</title>
     <style>
 
+
     body {
-      padding-top: 250px;
+      padding-top: 10%;
     }
+
+
     </style>
 
 
@@ -27,9 +30,9 @@
 
   </head>
 
-  <body>
+  <body style="background-color:#26819f">
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar navbar-inverse navbar-fixed-top " style="background-color:#04184a">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -51,35 +54,35 @@
     </nav>
     <div class="container " >
       <div class="col-xs-12" >
-        <h1 class="text-center">Emplea!</h1>
-        <p class="text-center">
+        <h1 class="text-center" style="color:#ffffff">Emplea!</h1>
+        <p class="text-center" style="color:#ffffff">
         Este sitio tiene un fin de emplear a aquellas personas que cuentan con algun oficio pero por distintas razones no cuentan con un empleo.
         </p>
-        <p class="text-center">
+        <p class="text-center" style="color:#ffffff">
           Si buscas ayuda a alguien  esta es la solucion , aqui podras buscar y encontrar personas con gran variedad de oficios listos para emplearse y que necesitan de ti
         </p>
       </div>
       <div class="col-xs4">
-        <div class="container">
+        <div class="container" style="padding-top: 150px;">
 
           <div class="starter-template ">
-            <h1 class="text-center col-xs-12">Primer paso</h1>
+            <h1 class="text-center col-xs-12" style="color:#ffffff">Primer paso</h1>
             <form>
 
           <div class="form-group row">
-            <label for="inputUsuario" class="col-xs-2 form-control-label">Nombre de usuario:</label>
+            <label for="inputUsuario" class="col-xs-2 form-control-label" style="color:#ffffff">Nombre de usuario:</label>
             <div class="col-xs-10">
               <input type="email" class="form-control" id="inputEmail3" placeholder="Usuario">
             </div>
           </div>
           <div class="form-group row">
-            <label for="inputEmail3" class="col-xs-2 form-control-label">Email</label>
+            <label for="inputEmail3" class="col-xs-2 form-control-label" style="color:#ffffff">Email:</label>
             <div class="col-xs-10">
               <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
             </div>
           </div>
           <div class="form-group row">
-            <label for="inputPassword3" class="col-xs-2 form-control-label">Contraseña</label>
+            <label for="inputPassword3" class="col-xs-2 form-control-label" style="color:#ffffff">Contraseña:</label>
             <div class="col-xs-10">
               <input type="password" class="form-control" id="inputPassword3" placeholder="Contraseña">
             </div>
