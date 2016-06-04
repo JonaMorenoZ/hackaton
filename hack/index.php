9eae4c31feb6dec67194a821b07fc09580000431
Jorge Jonathan Moreno Zúñiga <jonathan@VPCEA45FL>
2016-06-03 19:05:29 -0600

    Commit arreglos css

--- hack/index.php
+++ hack/index.php
@@ -13,25 +13,12 @@
 
     <title>Empleo</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="./css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <!--link href="./Starter Template for Bootstrap_files/ie10-viewport-bug-workaround.css" rel="stylesheet"-->
-
-    <!-- Custom styles for this template -->
-    <link href="./css/template.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <!--script src="./Starter Template for Bootstrap_files/ie-emulation-modes-warning.js"></script-->
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
       <script type="text/javascript" src="./js/Chart.min.js"></script>
+      <script src="./js/jquery.min.js"></script>
+      <script src="./js/bootstrap.min.js"></script>
+      <link href="./css/bootstrap.min.css" rel="stylesheet">
+      <link href="./css/index.css" rel="stylesheet">
+
 
   </head>
 
@@ -59,28 +46,13 @@
     </nav>
 
     <div class="container">
-
-      <div class="starter-template">
-        <h1>Bienvenido</h1>
+      <div class="row">
+        <div class="col-xs-4">
+          <button type="button" class="btn btn-primary btn-center">Primary</button>
+        </div>
+        <div class="col-xs-4">
+          <button type="button" class="btn btn-secondary btn-center">Secondary</button>
+        </div>
       </div>
-      <canvas id="chart-start">
-      </canvas>
-      <script type="text/javascript" src="./js/chart-start.js"></script>
-
-
-    </div><!-- /.container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="./js/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="./js/jquery.min.js"><\/script>')</script>
-    <script src="./js/bootstrap.min.js"></script>
-
-    <!--script type="text/javascript" src="./js/Chart.bundle.min.js"></script-->
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <!--script src="./Starter Template for Bootstrap_files/ie10-viewport-bug-workaround.js"></script-->
-
-
+    </div>
 </body></html>
