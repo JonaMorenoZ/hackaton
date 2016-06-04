3ecfd02ae4a0f648fca967afa9a908a99ef00b69
Daniel Rodríguez Gamboa <daniel@n56v>
2016-06-03 20:35:14 -0600

    Quite la caca de index.css

---
+++ hack/registro.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+
+<html lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://getbootstrap.com/favicon.ico">
+
+    <title>Empleo</title>
+    <style>
+    .container {
+            width: 75%;
+            margin-left: auto;
+            margin-right: auto;
+          }
+    </style>
+
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/index.css" rel="stylesheet">
+
+    <script type="text/javascript" src="./js/Chart.min.js"></script>
+
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://getbootstrap.com/examples/starter-template/#">Hackaton</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="http://getbootstrap.com/examples/starter-template/#">Inicio</a></li>
+            <li><a href="about.php">Acerca de</a></li>
+            <li><a href="http://getbootstrap.com/examples/starter-template/#contact">Contacto</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+    <div class="container"  >
+      <div class="col-xs-12" >
+        <h1 class="text-center">Emplea!</h1>
+        <p class="text-center">
+        Este sitio tiene un fin de emplear a aquellas personas que cuentan con algun oficio pero por distintas razones no cuentan con un empleo.
+        </p>
+        <p class="text-center">
+          Si buscas ayuda a alguien  esta es la solucion , aqui podras buscar y encontrar personas con gran variedad de oficios listos para emplearse y que necesitan de ti
+        </p>
+      </div>
+      <div class="col-xs4">
+        <div class="container">
+
+          <div class="starter-template ">
+            <h1 class="text-center col-xs-12">Primer paso</h1>
+            <form>
+
+          <div class="form-group row">
+            <label for="inputUsuario" class="col-xs-2 form-control-label">Nombre de usuario:</label>
+            <div class="col-xs-10">
+              <input type="email" class="form-control" id="inputEmail3" placeholder="Usuario">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputEmail3" class="col-xs-2 form-control-label">Email</label>
+            <div class="col-xs-10">
+              <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword3" class="col-xs-2 form-control-label">Contraseña</label>
+            <div class="col-xs-10">
+              <input type="password" class="form-control" id="inputPassword3" placeholder="Contraseña">
+            </div>
+          </div>
+          <div class="form-group row">
+            <div class="col-xs-offset-2 col-sm-10">
+              <button type="submit" class="btn btn-secondary">Ingresa</button>
+            </div>
+          </div>
+        </form>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./js/jquery.min.js"></script>
+
+    <script src="./js/bootstrap.min.js"></script>
+
+    <!--script type="text/javascript" src="./js/Chart.bundle.min.js"></script-->
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <!--script src="./Starter Template for Bootstrap_files/ie10-viewport-bug-workaround.js"></script-->
+
+
+</body>
+</html>
