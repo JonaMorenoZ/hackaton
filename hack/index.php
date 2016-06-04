cc42fa6c19b6727348004a682582909aabbd45be
Daniel Rodríguez Gamboa <daniel@n56v>
2016-06-04 02:07:52 -0600

    Version 2.0 Supachagia

--- hack/index.php
+++ hack/index.php
@@ -1,30 +1,38 @@
 <!DOCTYPE html>
-<!-- saved from url=(0050)http://getbootstrap.com/examples/starter-template/ -->
+
 <html lang="es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-    <!--meta http-equiv="X-UA-Compatible" content="IE=edge"-->
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="http://getbootstrap.com/favicon.ico">
 
     <title>Empleo</title>
+    <style>
 
-      <script type="text/javascript" src="./js/Chart.min.js"></script>
-      <script src="./js/jquery.min.js"></script>
-      <script src="./js/bootstrap.min.js"></script>
-      <link href="./css/bootstrap.min.css" rel="stylesheet">
-      <link href="./css/index.css" rel="stylesheet">
 
+    body {
+      padding-top: 10%;
+    }
+
+
+    </style>
+
+
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/index.css" rel="stylesheet">
+
+    <script type="text/javascript" src="./js/Chart.min.js"></script>
 
   </head>
 
-  <body>
+  <body style="background-color:#26819f">
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar navbar-inverse navbar-fixed-top " style="background-color:#04184a">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -33,26 +41,70 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="http://getbootstrap.com/examples/starter-template/#">Project name</a>
+          <a class="navbar-brand" href="index.php">Hackaton</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="http://getbootstrap.com/examples/starter-template/#">Home</a></li>
-            <li><a href="http://getbootstrap.com/examples/starter-template/#about">About</a></li>
-            <li><a href="http://getbootstrap.com/examples/starter-template/#contact">Contact</a></li>
+            <li class="active"><a href="index.php">Inicio</a></li>
+            <li><a href="about.php">Acerca de</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
     </nav>
-
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-4">
-          <button type="button" class="btn btn-primary btn-center">Primary</button>
-        </div>
-        <div class="col-xs-4">
-          <button type="button" class="btn btn-secondary btn-center">Secondary</button>
-        </div>
-      </div>
+    <div class="container " >
+      <div class="starter-template ">
+        <h1 class="text-center col-xs-12" style="color:#ffffff">Inicia sesion</h1>
     </div>
-</body></html>
+            <form>
+
+          <div class="form-group row">
+            <label for="inputUsuario" class="col-xs-2 form-control-label" style="color:#ffffff">Nombre de usuario:</label>
+            <div class="col-xs-10">
+              <input type="text" class="form-control" id="inputEmail3" placeholder="Usuario">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword3" class="col-xs-2 form-control-label" style="color:#ffffff">Contraseña:</label>
+            <div class="col-xs-10">
+              <input type="password" class="form-control" id="inputPassword3" placeholder="Contraseña">
+            </div>
+          </div>
+          <div class="form-group row">
+            <div class="col-xs-offset-2 col-sm-10">
+              <button type="submit" class="btn btn-secondary">Ingresa</button>
+            </div>
+          </div>
+        </form>
+        <div class="col-xs-12" >
+          <h1 class="text-center" style="color:#ffffff">Da el primer paso</h1>
+          <p class="text-center" style="color:#ffffff">
+          Si eres desempleado o buscas el ayudar o emplear a alguien este es el mejor sitio para ti
+          </p>
+          <p class="text-center" style="color:#ffffff">
+            Da tu primer paso ahora, no es necesario que veas el camino completo. Solo da el primer paso. El resto irá apareciendo a medida que camines.
+          </p>
+          <div class="col-xs-12">
+            <button type="button" class="btn btn-primary col-xs-6" style="background-color:#ff6384; height:80px; font-size:25px"><a class="active" href="regext.php" style="color:#ffffff">Empleate!</a></button>
+            <button type="button" class="btn btn-primary col-xs-6" style="background-color:#36a2eb; height:80px; font-size:25px"><a class="active" href="registro.php" style="color:#ffffff">Emplea!</a></button>
+          </div>
+        </div>
+        <p class="text-center" style="color:#ffffff; font-size:5px">
+      Copyright © [2016, pj: "2010-2016"] [Muy tu pedo si te asaltan, ej: "No nos hacemos cargo si te alsaltan con filero .. etc!"]
+        </p>
+
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./js/jquery.min.js"></script>
+
+    <script src="./js/bootstrap.min.js"></script>
+
+    <!--script type="text/javascript" src="./js/Chart.bundle.min.js"></script-->
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <!--script src="./Starter Template for Bootstrap_files/ie10-viewport-bug-workaround.js"></script-->
+
+
+</body>
+</html>
--- hack/registro.php
+++ hack/registro.php
@@ -41,13 +41,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="http://getbootstrap.com/examples/starter-template/#">Hackaton</a>
+          <a class="navbar-brand" href="index.php">Hackaton</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="http://getbootstrap.com/examples/starter-template/#">Inicio</a></li>
+            <li class="active"><a href="index.php">Inicio</a></li>
             <li><a href="about.php">Acerca de</a></li>
-            <li><a href="http://getbootstrap.com/examples/starter-template/#contact">Contacto</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
