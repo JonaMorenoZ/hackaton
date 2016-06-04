82fe20a8f7b1f61a0b1dc9b4a92f10750b7d98ae
Daniel Rodríguez Gamboa <daniel@n56v>
2016-06-04 03:29:17 -0600

    Lol

---
+++ hack/inuser.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+
+<html lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://getbootstrap.com/favicon.ico">
+    <title>Empleo</title>
+    <style>
+    body {
+      padding-top: 10%;
+    }
+    </style>
+
+
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/index.css" rel="stylesheet">
+
+    <script type="text/javascript" src="./js/Chart.min.js"></script>
+
+  </head>
+
+  <body style="background-color:#26819f">
+
+    <nav class="navbar navbar-inverse navbar-fixed-top " style="background-color:#04184a">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php" style="font-size:30px">Hackaton</a></li>
+            <li><a href="mapa.php">Mapa</a></li>
+            <li><a href="search.php">Buscador</a></li>
+            <li><a href="regmin.php">Empleate!</a></li>
+            <li><a href="about.php">Acerca de</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+            <li><a href="index.php">Salir</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+    <div class="container " >
+      
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./js/jquery.min.js"></script>
+
+    <script src="./js/bootstrap.min.js"></script>
+
+    <!--script type="text/javascript" src="./js/Chart.bundle.min.js"></script-->
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <!--script src="./Starter Template for Bootstrap_files/ie10-viewport-bug-workaround.js"></script-->
+
+
+</body>
+</html>
